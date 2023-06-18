1168049e739ff1f300b8245df76648d61924f69d
CD2 <18058378+CableDorado2@users.noreply.github.com>
2023-06-17 21:39:23 -0430

    Update 1.3

--- saved/stats_sav.lua
+++ saved/stats_sav.lua
@@ -1,9 +1,9 @@
 data.arcadeUnlocks = true
 data.survivalUnlocks = false
 data.coins = 15
-data.playTime = 8608.747
-data.favoriteChar = "Kung Fu Man"
-data.favoriteStage = "Random"
+data.playTime = 9842.417
+data.favoriteChar = "Colossus Magna"
+data.favoriteStage = "Training Room"
 data.victories = 17
 data.defeats = 47
 data.preferredMode = "WIP"
