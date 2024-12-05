92921511c962ecf0147750b34b06c3df526601a2
CD2 <18058378+CableDorado2@users.noreply.github.com>
2024-12-05 13:13:14 -0400

    Delete data/events directory

--- data/events/event1.lua
+++
@@ -1,7 +0,0 @@
-data.p2In = 1
-data.p2TeamMenu = {mode = 0, chars = 1}
-data.p2Char = {"Event 1"}
-textImgSetText(txt_mainSelect, 'CHARACTER SELECT')
-setRoundTime(-1)
-setRoundsToWin(1)
-f_selectSimple()
